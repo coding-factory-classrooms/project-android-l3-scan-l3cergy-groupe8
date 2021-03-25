0d7e172441afff2d6789e9f03763306516e3ad29
Antonin Pulby <antonin.pulby@edu.itescia.fr>
2021-03-25 13:15:28 +0100

    Récupération de données depuis l'API openFoodFacts fonctionnelle avec implémentation de retrofit, moshi + OkHttp pour les logs

--- app/src/main/java/com/sushi/izishopping/Models.kt
+++ app/src/main/java/com/sushi/izishopping/Models.kt
@@ -1,7 +1,22 @@
 package com.sushi.izishopping
 
+import com.squareup.moshi.Json
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+data class FoodResponse(val product: ProductApi)
+
+data class ProductApi(val code: String, val product_name: String, val image_url: String, val nutrition_grades: String, val ingredients_text_fr: String)
+
 data class Food(
-    val barcode: String,
-    val name: String,
-    val imgLink: String,
+    @field:Json(name="code") val barcode: String,
+    @field:Json(name="product_name") val name: String,
+    val dateScan: String
 )
+
+interface FoodApi {
+    @GET("v0/product/{barcode}.json")
+    fun foodInformation(@Path("barcode") barcode: String): Call<FoodResponse>
+}
+
--- app/src/main/java/com/sushi/izishopping/scanner/ScannerViewModel.kt
+++ app/src/main/java/com/sushi/izishopping/scanner/ScannerViewModel.kt
@@ -1,9 +1,22 @@
 package com.sushi.izishopping.scanner
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.sushi.izishopping.FoodApi
+import com.sushi.izishopping.FoodResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 private const val TAG = "ScannerViewModel"
 
@@ -17,10 +30,36 @@ sealed class ScannerViewModelState(
 class ScannerViewModel : ViewModel() {
 
     private val state = MutableLiveData<ScannerViewModelState>()
-
     fun getInfos() : LiveData<ScannerViewModelState> = state
 
     fun findFoodInfos(barcode: String) {
-        Log.i(TAG, "findFoodInfos: $barcode")
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()) .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://world.openfoodfacts.org/api/")
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(FoodApi::class.java)
+
+        retrofit.foodInformation(barcode).enqueue(object : Callback<FoodResponse> {
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onResponse(call: Call<FoodResponse>, response: Response<FoodResponse>) {
+                Log.i(TAG, "onResponse: response = ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<FoodResponse>, t: Throwable) {
+                Log.i(TAG, "onFailure: P'tite erreur")
+                t.printStackTrace()
+            }
+
+        })
     }
 }
