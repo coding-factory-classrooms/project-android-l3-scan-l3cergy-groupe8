e5ea021745fb33f1bb2904b64935f554b7357e45
Sushi <joankucukoglu@gmail.com>
2021-03-26 04:13:44 +0100

    [JK] : Delete MainActivity and fix import error

--- app/src/main/java/com/sushi/izishopping/activity/ScannerActivity.kt
+++ app/src/main/java/com/sushi/izishopping/activity/ScannerActivity.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.Observer
 import com.google.zxing.integration.android.IntentIntegrator
 import com.sushi.izishopping.App
 import com.sushi.izishopping.databinding.ActivityScannerBinding
-import com.sushi.izishopping.scanner.ScannerViewModel
+import com.sushi.izishopping.viewmodel.ScannerViewModel
 
 private const val TAG = "ScannerActivity"
 
